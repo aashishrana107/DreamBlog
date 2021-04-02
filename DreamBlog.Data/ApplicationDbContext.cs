7b454728d6467f2d3a6694ac2f024e2352611bd2
Ashish Kumar <aashishrana107@gmail.com>
2021-04-03 00:13:49 +0530

    Add project files.

---
+++ DreamBlog.Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DreamBlog.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DreamBlog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
